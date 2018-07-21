6a4711b2311a14515c44dab391c92283bc5c6f96
chao <1095087479@qq.com>
2018-07-21 18:07:57 +0800

    deploy

--- deploy.py
+++ deploy.py
@@ -73,9 +73,15 @@ def install_runner():
     # os.system("sudo mkdir /etc/gitlab-runner")
     # assert s0 == 0, "创建runner配置文件夹失败"
     s1 = os.system("docker run --rm -t -i -v /etc/gitlab-runner:/etc/gitlab-runner gitlab/gitlab-runner register \
-                   -n -u https://gitlab.com/ -r cyVyXLcrRxtgwGz_r2_q --executor shell \
+                   -n -u https://gitlab.com/ -r cyVyXLcrRxtgwGz_r2_q --executor docker --docker-image docker\
                    --tag-list {}".format(os.getenv("CUSCODE")))
     assert s1 == 0, "runner配置失败"
+    os.system("sudo chmod 777 /etc/gitlab-runner/config.toml")
+    with open('/etc/gitlab-runner/config.toml', 'r') as f:
+        s = f.read()
+        a = s.replace(' volumes = ["/cache"]', 'volumes = ["/var/run/docker.sock:/var/run/docker.sock", "/cache"]')
+    with open('/etc/gitlab-runner/config.toml', 'w') as f:
+        f.write(a)
     s2 = os.system("docker run -d --name gitlab-runner --restart always \
                    -v /etc/gitlab-runner:/etc/gitlab-runner \
                    -v /var/run/docker.sock:/var/run/docker.sock \
