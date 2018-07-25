e60a74be8e8599c3b7cdbe988745fa30f0a11b2b
chao <1095087479@qq.com>
2018-07-25 14:54:43 +0800

    docker ali mirrors

--- install_docker.py
+++ install_docker.py
@@ -2,10 +2,20 @@ import os
 
 
 def install_docker():
-    step1 = os.system("wget -qO- https://get.docker.com/ | sh")
-    assert step1 == 0, "docker 安装失败"
-    step2 = os.system('sudo usermod -aG docker huansi')
-    assert step2 == 0, "docker 用户组更改失败"
+    # step1 = os.system("wget -qO- https://get.docker.com/ | sh")
+    # assert step1 == 0, "docker 安装失败"
+    # step2 = os.system('sudo usermod -aG docker huansi')
+    # assert step2 == 0, "docker 用户组更改失败"
+    os.system("sudo mkdir -p /etc/docker")
+    step3 = os.system("""
+    sudo tee /etc/docker/daemon.json <<-'EOF'
+{
+  "registry-mirrors": ["https://m6wlkecl.mirror.aliyuncs.com"]
+}
+EOF
+    """)
+    os.system("sudo systemctl daemon-reload")
+    os.system("sudo systemctl restart docker")
     print("docker安装成功")
 
 
