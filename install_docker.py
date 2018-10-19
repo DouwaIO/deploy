e3a3884e8eda1f74904fc3a12493834423557e8e
chao <1095087479@qq.com>
2018-10-19 22:48:02 +0800

    add install k8s

--- install_docker.py
+++ install_docker.py
@@ -61,7 +61,23 @@ EOF
     print("docker安装成功")
 
 
+def install_compose():
+    s0 = os.system("sudo apt update")
+    assert s0 == 0, "apt 更新失败"
+    s1 = os.system("sudo apt install -y -qq --no-install-recommends python-pip >/dev/null")
+    assert s1 == 0, "安装pip失败"
+    s2 = os.system("sudo apt install -y -qq --no-install-recommends python3-pip >/dev/null")
+    assert s2 == 0, "安装pip3失败"
+    s = os.system('sudo pip install setuptools')
+    assert s==0, "setuptools 安装失败"
+    s3 = os.system("sudo pip install docker-compose")
+    assert s3 == 0, "安装docker-compose失败"
+    s4 = os.system("sudo docker-compose --version")
+    assert s4 == 0, "docker-compose 启动失败"
+    print("安装docker-compose成功")
+
 if __name__ == "__main__":
-    init_env()
+    # init_env()
     install_docker()
+    install_compose()
     print("请重启虚拟机")
